ad74aaacf4d9fe92309233a00f9a7b5d54044f86
DIOGOBRODRIGUES <diogo.borbar@gmail.com>
2022-08-27 15:50:52 -0300

    inserindo funcao javascript

---
+++ AppComentario/app.js
@@ -0,0 +1,24 @@
+var main = function(){
+	"use strict";
+	var addCommentFromInputBox = function (){
+		if($(".comment-input input").val() !== ""){
+			var $new_comment = $("<p>").text($(".comment-input input").val());
+			$new_comment.hide();
+			$(".comments").append($new_comment);
+			$new_comment.fadeIn();
+			$(".comment-input input").val("");
+		}
+	}
+	
+	$(".comment-input button").on("click", function(event){
+		addCommentFromInputBox();
+	});
+	
+	$(".comment-input input").on("keypress", function(event){
+		if(event.keyCode === 13){
+			addCommentFromInputBox();
+		}
+		
+	});
+};
+$(document).ready(main)
